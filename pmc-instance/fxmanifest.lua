b23cf251d7535fc46a77c722f4f091232ba847d1
Kaan <81617346+Kaanhehe@users.noreply.github.com>
2024-01-11 16:30:15 +0100

    made it work with garage i hope

--- pmc-instance/fxmanifest.lua
+++ pmc-instance/fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.0.0'
 
 server_only 'yes'
 
-debug_log 'yes' -- set this to 'no' to disable output log messages
+debug_log 'no' -- set this to 'no' to disable output log messages
 debug_message '[INSTANCE]: Player %s entered instance %s'
 
 server_script 'server/misc.lua'
--- pmc-instance/server/misc.lua
+++ pmc-instance/server/misc.lua
@@ -1,3 +1,5 @@
+-- Idk if it was like a trend to always use _ for every variable name, but I'm not doing that.
+-- Its easier to read assembly than it is to read code with _ everywhere.
 local GERB = assert(GetEntityRoutingBucket)
 local GPRB = assert(GetPlayerRoutingBucket)
 local SERB = assert(SetEntityRoutingBucket)
@@ -5,67 +7,71 @@ local SPRB = assert(SetPlayerRoutingBucket)
 
 _G.Instances = {}
 
-_G.U = {
+_G.Utilities = {
 	isDebugEnabled = (GetResourceMetadata(GetCurrentResourceName(), 'debug_log') == 'yes'),
 	debugFormatted = GetResourceMetadata(GetCurrentResourceName(), 'debug_message') or '[INSTANCE] Player %s entered instance %s'
 }
 setmetatable(U, {
 	__index = {
-		DebugMessage = function(self, _s, _in)
+		DebugMessage = function(self, playerId, instanceId)
 			if self.isDebugEnabled then
-				print(self.debugFormatted:format(_s, _in))
+				print(self.debugFormatted:format(playerId, instanceId))
 			end
 		end,
 		Ensure = function(self, o, t)
 			assert(type(o) == t, 'invalid Lua type, expected '..t..', got '..type(o))
 			return true
 		end,
-		EnsureInstance = function(self, _in)
-			self:Ensure(_in, 'number')
-			if Instances[_in] == nil then
-				Instances[_in] = {
+		EnsureInstance = function(self, instanceId)
+			self:Ensure(instanceId, 'number')
+			if Instances[instanceId] == nil then
+				Instances[instanceId] = {
 					players = {},
 					entities = {}
 				}
-			elseif Instances[_in].players == nil then
-				Instances[_in].players = {}
-			elseif Instances[_in].entities == nil then
-				Instances[_in].entities = {}
+			elseif Instances[instanceId].players == nil then
+				Instances[instanceId].players = {}
+			elseif Instances[instanceId].entities == nil then
+				Instances[instanceId].entities = {}
 			end
 			return true
 		end,
-		EnsureAddPlayer = function(self, _in, _s)
-			if self:Ensure(_in, 'number') and self:Ensure(_s, 'number') then
-				if self:EnsureInstance(_in) then
-					if GPRB(_s) == 0 then
-						table.insert(Instances[_in].players, _s)
-						SPRB(_s, _in)
-						self:DebugMessage(_s, _in)
+		EnsureAddPlayer = function(self, instanceId, playerId)
+			if self:Ensure(instanceId, 'number') and self:Ensure(playerId, 'number') then
+				if self:EnsureInstance(instanceId) then
+					if GPRB(playerId) == 0 then
+						table.insert(Instances[instanceId].players, playerId)
+						SPRB(playerId, instanceId)
+						self:DebugMessage(playerId, instanceId)
 					else
-						self:EnsureChangePlayer(_in, _s)
+						self:EnsureChangePlayer(instanceId, playerId, true) -- add player to garage via invite or jobgarage
 					end
 				end
 			end
 		end,
-		EnsureRemovePlayer = function(self, _s)
-			if self:Ensure(_s, 'number') then
-				self:EnsureChangePlayer(0, _s)
+		EnsureRemovePlayer = function(self, instanceId, playerId)
+			if self:Ensure(playerId, 'number') then
+				self:EnsureChangePlayer(instanceId, playerId, false)
 			end
 		end,
-		EnsureChangePlayer = function(self, _in, _s)
-			if self:Ensure(_in, 'number') and self:Ensure(_s, 'number') then
-				local _c_in = GPRB(_s)
-				if self:EnsureInstance(_in) and self:EnsureInstance(_c_in) then
-					if _c_in ~= 0 then
-						for _i, _v in ipairs(Instances[_in].players) do
-							if _v == _s then
-								Instances[_in].players[_i] = nil
-								break
+		EnsureChangePlayer = function(self, instanceId, playerId, addtoInstance)
+			if self:Ensure(instanceId, 'number') and self:Ensure(playerId, 'number') then
+				local currentInstance = GPRB(playerId)
+				if self:EnsureInstance(instanceId) and self:EnsureInstance(currentInstance) then
+					if currentInstance ~= 0 then
+						if addtoInstance then
+							Instances[instanceId].players[#Instances[instanceId].players + 1] = playerId
+						else
+							for i, v in ipairs(Instances[instanceId].players) do
+								if v == playerId then
+									Instances[instanceId].players[i] = nil
+									break
+								end
 							end
 						end
-						table.insert(Instances[_in].players, _s)
-						SPRB(_s, _in)
-						self:DebugMessage(_s, _in)
+						instanceId = addtoInstance and instanceId or 0
+						SPRB(playerId, instanceId)
+						self:DebugMessage(playerId, instanceId)
 					end
 				end
 			end
